2383eb846881507d07fac536ca6b0ca150dd0229
Tyler Franklin <tylerfranklin@protonmail.com>
2022-01-08 23:46:00 +0800

    Create PunkShequ.justlists.ts

---
+++ json/token/dak/PunkShequ.justlists.ts
@@ -0,0 +1,28 @@
+{
+    "name": "PunkShequ",
+    "tokens": [
+       {
+            "chainId": 1,
+            "address": "TWuWBVJB5ezFVJEGDR1HnXB6nYWThv8Lmw",
+            "name": "DAK Coin",
+            "symbol": "DAK",
+            "decimals": 18,
+            "logoURI": "https://tyler-franklin.github.io/metadata/json/token/dak/daklogo.png"
+        },
+        {
+            "chainId": 1,
+            "address": "TAVvpPJxcizhauJuwgHQnDKDbjFYAGKseC",
+            "name": "BOS Coin",
+            "symbol": "BOS",
+            "decimals": 18,
+            "logoURI": "https://tyler-franklin.github.io/metadata/json/token/bos/BOS.eacd757c.png"
+        }
+    ],
+    "logoURI": "https://tyler-franklin.github.io/metadata/json/token/bos/BOS.eacd757c.png",
+    "version": {
+        "patch": 1,
+        "major": 0,
+        "minor": 0
+    },
+    "timestamp": 1641537981000
+}
